a5ecaa43b3fd73fe7a006536a30af8b7563c3243
hunter0892 <106370926+hunter0892@users.noreply.github.com>
2022-06-03 00:19:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function mooncakeSays(message) {
   let mooncake = '(o_o)';
+  
+  
+  
 
   if ( !message ) {
     return `${mooncake} chookity?`;
@@ -8,4 +11,4 @@ function mooncakeSays(message) {
   return `${mooncake} ${message}!`;
 }
 
-module.exports.mooncakeSays = mooncakeSays;
+module.exports.mooncakeSays = mooncakeSaysew;
